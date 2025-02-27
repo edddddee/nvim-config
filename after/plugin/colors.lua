b918cf25c07a8bd86ad63bc443262a304e0bcf56
SinisterGaze <edvkar98@gmail.com>
2025-02-27 19:51:41 +0100

    using terminal bg color and making vim 'transparent'

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,3 +1,6 @@
--- Set background color to black
+-- Transparent background color (use terminal settings)
 vim.cmd("hi Normal guibg=none")
+
+-- Black background color
+--vim.cmd("hi Normal guibg=#000000")
 
